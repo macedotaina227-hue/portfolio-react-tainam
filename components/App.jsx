ae07af685a58c381612119ee7707b3ab53f146e1
macedotaina227-hue <macedotaina227@gmail.com>
2026-04-15 10:16:20 -0300

    Create App.jsx

---
+++ components/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
